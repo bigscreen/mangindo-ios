a267baf3c0927788cb8cfce7a9055665fe15c1a9
Gallant Pratama <gallantpratama@go-jek.com>
2017-02-17 17:35:05 +0700

    Add new release collection view with dummy data

---
+++ Mangindo/NewReleasedCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NewReleasedCollectionViewCell.swift
+//  Mangindo
+//
+//  Created by Gallant Pratama on 2/17/17.
+//  Copyright © 2017 Gallant Pratama. All rights reserved.
+//
+
+import UIKit
+
+class NewReleasedCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
