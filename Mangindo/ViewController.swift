49cbf18a97395ec49eb518836f182a7280140d26
Gallant Pratama <gallantpratama@go-jek.com>
2017-04-20 22:40:12 +0700

    Set collection view cell width dynamically

--- Mangindo/ViewController.swift
+++ Mangindo/ViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
-    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    @IBOutlet weak var newReleaseCollectionView: UICollectionView!
+    
+    let reuseIdentifier = "newReleaseCell" // also enter this string as the cell identifier in the storyboard
+    
     var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +28,16 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     
     // MARK: - UICollectionViewDataSource protocol
+    
+    // set width of collection view cell
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collViewWidth = newReleaseCollectionView.frame.size.width
+        let cellWidth = (collViewWidth / 3) - 6;
+        let cellHeight = cellWidth * 1.5
+        print("collectionViewWidth= \(collViewWidth), cellWidth=\(cellWidth), cellHeight=\(cellHeight)")
+        return CGSize(width: cellWidth, height: cellHeight)
+        
+    }
     
     // tell the collection view how many cells to make
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
